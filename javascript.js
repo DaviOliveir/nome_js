74f78763149b9212e780d215ca4af2049bfe285f
Davi José batista Lima de Oliveira <112220168+DaviOliveir@users.noreply.github.com>
2023-04-13 21:06:03 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+let nome = window.prompt('Digite seu nome');
+let sobrenome = window.prompt('Digite seu Sobrenome');
+
+window.alert(`Nome Completo: ${nome} ${sobrenome} \n
+Nome Catálogo: ${sobrenome.toUpperCase()}, ${nome}`)
